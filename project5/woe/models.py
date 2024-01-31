68e71e17cd98c3c34860f79e43361a9ba3c79380
mrcrowley42 <mattcrowley42@me.com>
2024-02-01 07:06:49 +1100

    Update models.py

--- project5/woe/models.py
+++ project5/woe/models.py
@@ -3,13 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-class Observation(models.Model):
-    """ Defines the Observations table. """
-    pass
-
-
 class Source(models.Model):
     """ Defines the Sources Table """
     name = models.CharField(max_length=255)
     wmo_id = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
+    
+class Observation(models.Model):
+    """ Defines the Observations table. """
+    location = models.ForeignKey(Source, default=1, on_delete=models.SET_DEFAULT)
