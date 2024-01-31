6101b3e2168a73d0fd5e93cd4e66e053ff69eb0d
matt-crowley <mattcrowley42@gmail.com>
2024-01-31 15:32:03 +1100

    created source model and migration to populate its table

--- project5/project5/settings.py
+++ project5/project5/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'woe',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- project5/woe/models.py
+++ project5/woe/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Observation(models.Model):
+    """ Defines the Observations table. """
+    pass
+
+
+class Source(models.Model):
+    """ Defines the Sources Table """
+    name = models.CharField(max_length=255)
+    wmo_id = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
