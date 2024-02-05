e240122f86f08a1623ec878a69577b5d90becb8e
Bryce VE <brycevanewyk4@gmail.com>
2024-02-05 14:06:28 +1100

    index view working

--- project5/project5_dj/settings.py
+++ project5/project5_dj/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'project5_dj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['woe.templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
