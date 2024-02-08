375a71add77dd0c0ddd9647c062c67e5af44ef64
matt-crowley <mattcrowley42@gmail.com>
2024-02-08 15:18:32 +1100

    created test to test api entry works

--- project5/woe/tests/tests.py
+++ project5/woe/tests/tests.py
@@ -1,30 +1,28 @@
-import pytest
 import json
-# from django.http import HttpResponse
-# from django.test import TestCase
-# from django.core import serializers
-# from project5.logging_module import logging_script
-from project5.woe.models import Source, Observation
-# from project5.scripts.api_functions import retrieve_urls
+from django.http import HttpResponse
+from django.test import TestCase
+from django.core import serializers
+from project5.logging_module import logging_script
+from woe.models import Source, Observation
+from project5.scripts.api_functions import *
 
 
-@pytest.fixture
-def database():
-    # data = serializers.serialize('json', Source.objects.all()[-1])
-    # print(data)
-    return "hello"
-
-
-def test_what(database):
-    assert database == "hello2"
-
-
-def test_api_to_db():
-    """Test that json data is correctly recorded in database."""
-    with open('tests/data/dummy.json') as fh:
-        data = json.load(fh)
-    api_data = [line for line in data['observations']['data']][0]
-    assert 1 == api_data
+class ApiTests(TestCase):
+    def test_api_to_db(self):
+        """Test that json data is correctly recorded in database."""
+        with open('woe/tests/data/dummy.json') as fh:
+            data = json.load(fh)
+        api_data = [line for line in data['observations']['data']][0]
+        self.assertEqual(2, 2)
+        wmo_dict = create_wmo_dict()
+        obs = enter_observation(api_data, wmo_dict)
+        obs.save()
+        last_entry = Observation.objects.all().order_by('-id')[0]
+        self.assertEqual(last_entry.local_date_time_full, int(api_data['local_date_time_full']))
+        self.assertEqual(last_entry.air_temp, api_data['air_temp'])
+        self.assertEqual(last_entry.dewpt, api_data['dewpt'])
+        self.assertEqual(last_entry.wind_dir, api_data['wind_dir'])
+        self.assertEqual(last_entry.wind_spd_kmh, api_data['wind_spd_kmh'])
 
 
 def test_filter_wmo():
