cae0269980899508bd6e264298ddb008bd9079aa
Matt <mattcrowley42@me.com>
2024-02-17 15:38:01 +1100

    refactored modal pop up function to woejs

--- project5/woe/static/woe/WOE_JS.js
+++ project5/woe/static/woe/WOE_JS.js
@@ -111,3 +111,23 @@ function create_chart_from_data(canvas_input, response) {
     //chart_data = get_data_example();
     //console.log(chart_data);
 }
+
+
+function openModalPopup(modalID, closeID){
+    // Get the modal
+    var modal = document.getElementById(modalID);
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName(closeID)[0];
+    // When the user clicks the button, open the modal
+    modal.style.display = "block";
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+        }
+    // When the user clicks anywhere outside the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            }
+        }
+   }
