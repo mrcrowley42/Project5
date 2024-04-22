d531f874f33a3dd3d159d9ce91f2a45050126784
Bryce VE <brycevanewyk4@gmail.com>
2024-04-23 09:30:39 +1000

    differnt routes using react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,34 @@ import './style.css'
 import ExampleList from './components/example-list';
 import MyButton from './components/example-button';
 import NavBar from './components/navigation-bar';
+import { BrowserRouter, Routes, Route, NavLink, Outlet } from 'react-router-dom';
+
+function Index() {
+  return (
+    <div className='content'>
+      <ExampleList />
+      <MyButton />
+    </div>
+  )
+}
+
+function Other() {
+  return (
+    <div>Othererer!</div>
+  )
+}
 
 export default function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <div className='content'>
-      <ExampleList />
-    <MyButton />
-    </div>
-    </div>  
+      <BrowserRouter>
+        <div className='App'>
+          <NavBar />
+
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/other" element={<Other />} />
+          </Routes>
+      </div>
+      </BrowserRouter>
   );
 }
-
