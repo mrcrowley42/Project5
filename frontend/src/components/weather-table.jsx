281b0fcd0f4868d7ca62d255606b18a5df952286
matt-crowley <mattcrowley42@gmail.com>
2024-04-24 14:43:58 +1000

    added pages

--- frontend/src/components/weather-table.jsx
+++ frontend/src/components/weather-table.jsx
@@ -1,45 +1,85 @@
 import React, {useState, useEffect} from "react";
-
+import axios from 'axios';
 
 export default function WeatherTable(){
 
-    const [examples, setWeather] = useState([]);
 
-    
+
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(false)
     useEffect(() => {
-
-        // fetch("https://jsonplaceholder.typicode.com/users")
-        fetch("http://127.0.0.1:8000/user?limit=10&wmo=2")
+      setLoading(true)
+      fetch("http://127.0.0.1:8000/user?limit=10&wmo=2")
         .then(response => response.json())
-        .then(data => setWeather(data))
-        .catch(err => console.log(err))},[])
+        .then(json => setUsers(json))
+        .finally(() => {
+          setLoading(false)
+        })
+    }, [])
 
-const rows = examples.map(example =>
+    return (
+<>
+        {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <>
+              <h1>Users</h1>
+              <table border={1}>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                </tr>
+                {users.map(user => (
 
-    //key={example.id}
-    <tr >
-        <td>{example.wmo_id}</td>
-        <td>{example.location}</td>
-        <td>{example.website}</td>
-        <td>{example.id}</td>
-    </tr>);
+                    
+                  <tr key={user.id}>
+                    <td>{user.wmo_id}</td>
+                    <td>{user.location}</td>
+                    <td>{user.observations}</td>
+                    
+                  </tr>
+                ))}
+              </table>
+            </>
+          )}
 
-return (
-<table>
-    <thead>
-    <tr>
-        <th>Name</th>
-        <th>E-mail</th>
-        <th>Site</th>
-        <th>ID</th>
-    </tr>
-    </thead>
-    <tbody>
-{rows}
-    </tbody>
-    
-
-</table>
-
+</>
     );
+
 }
+
+
+
+
+
+
+//     const [examples, setWeather] = useState([]);
+
+    
+//     useEffect(() => {
+
+//         axios.get('')
+//       .then(response => {
+//         setWeather(response.data.observations);
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   }, []);
+
+
+// // const rows = examples.map(example =>
+
+// //     //key={example.id}
+// //     <tr >
+// //         <td>{example.wmo_id}</td>
+// //         <td>{example.name}</td>
+// //         <td>{example.website}</td>
+// //         <td>{example.id}</td>
+// //     </tr>);
+
+// return (
+//    
+//     );
+// }
