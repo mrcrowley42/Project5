9af287a8dff121046209e9b2d8dfc367f8b1cf29
Bryce VE <brycevanewyk4@gmail.com>
2024-04-29 10:04:07 +1000

    fix table error being thrown

--- frontend/src/components/weather-table.jsx
+++ frontend/src/components/weather-table.jsx
@@ -2,11 +2,9 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios';
 
 export default function WeatherTable(){
-
-
-
     const [observations, setObservations] = useState([])
     const [loading, setLoading] = useState(false)
+
     useEffect(() => {
       setLoading(true)
       fetch("http://127.0.0.1:8000/user?limit=10&wmo=8")
@@ -15,35 +13,36 @@ export default function WeatherTable(){
         .finally(() => {
           setLoading(false)
         })
-    }, [])
+    }, [setObservations])
 
     return (
-<>
+        <>
         {loading ? 
             (<div>Loading...</div>) 
-            : 
-            (
-            <>
-              <h1>stuf</h1>
-              <table border={1}>
-                <tr>
-                  <th>air_temp</th>
-                  <th>dewpt</th>
-                  <th>formatted datetime</th>
-                </tr>
-                {observations.map(obs => (
+            :
+            (<>
+                <h1>stuf</h1>
+                <table border={1}>
+                    <thead>
+                        <tr>
+                        <th>air_temp</th>
+                        <th>dewpt</th>
+                        <th>formatted datetime</th>
+                        </tr>
+                    </thead>
 
+                    {observations.map(obs => (
+                    <tbody key={obs.id}>
+                        <tr>
+                        <td>{obs.air_temp}</td>
+                        <td>{obs.dewpt}</td>
+                        <td>{obs.formatted_datetime}</td>
+                        </tr>
+                    </tbody>
+                    ))}
                     
-                  <tr key={obs.id}>
-                    <td>{obs.air_temp}</td>
-                    <td>{obs.dewpt}</td>
-                    <td>{obs.formatted_datetime}</td>
-                    
-                  </tr>
-                ))}
-              </table>
-            </>
-            )
+                </table>
+            </>)
         }
         </>
     );
