2f807d8b3f317469b3df306e4f01b0bbb7651637
Bryce VE <brycevanewyk4@gmail.com>
2024-05-02 11:36:46 +1000

    remove broken thingo

--- project5/woe/static/woe/WOE_JS.js
+++ project5/woe/static/woe/WOE_JS.js
@@ -67,7 +67,7 @@ function show_selected(element) {
 
 function do_ajax(dropdown, div_id) {
     let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange examples.map(example =>= function () {
+    xhttp.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
         document.getElementById(div_id).innerHTML = '' + this.responseText;
     }
@@ -123,11 +123,11 @@ function openModalPopup(modalID, closeID){
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
-        }
+    }
     // When the user clicks anywhere outside the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
-            }
         }
-   }
+    }
+}
